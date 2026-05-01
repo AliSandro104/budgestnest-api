56ad1d8a688f0fc50b6a6c781451dd89fea55985
AliSandro104 <alisandro2000@hotmail.com>
2026-04-30 22:39:20 -0400

    first commit

---
+++ src/test/java/io/github/alisandro104/budgetnest_api/BudgetnestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alisandro104.budgetnest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetnestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
